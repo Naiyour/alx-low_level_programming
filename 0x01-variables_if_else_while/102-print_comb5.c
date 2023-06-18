d495e42cc252a0efd7def8ef5324c49025eca3a2
Naiyour <abimbolafayose@yahoo.com>
2023-06-18 16:20:48 -0700

    software

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-       	int i, j;
+	int i, j;
 
 	for (i = 0; i < 100; i++)
 	{
@@ -15,7 +15,7 @@ int main(void)
 			if (i < j)
 			{
 				putchar((i / 10) + 48);
-				putchar((i % 10) +48);
+				putchar((i % 10) + 48);
 				putchar(' ');
 				putchar((j / 10) + 48);
 				putchar((j % 10) + 48);
