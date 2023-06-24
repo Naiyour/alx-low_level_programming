ffb9f6fb5aaa6625afba6946903b8780dad23208
Naiyour <abimbolafayose@yahoo.com>
2023-06-23 18:02:13 -0700

    prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -11,17 +11,14 @@ int main(void)
 	long int n;
 	long int max;
 	long int i;
-
 	n = 612852475143;
 	max = -1;
-
 	while (n % 2 == 0)
 	{
 		max =2;
 		n /= 2;
 	}
-	
-	for (i = 3; i <= sqrt(n); i = i + 2)
+		for (i = 3; i <= sqrt(n); i = i + 2)
 	{
 		while ( n % i == 0)
 		{
@@ -32,6 +29,4 @@ int main(void)
 	if (n > 2)
 		max =n;
 	printf("%ld\n", max);
-	
-	return (0);
-}
+	return (0);}
